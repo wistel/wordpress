168d58951db0dd4b9a32946252721881f49315e4
yiki-computer <65933962@qq.com>
2016-01-01 22:26:47 +0800

    adsense

--- wp-content/themes/iconic-one/sidebar.php
+++ wp-content/themes/iconic-one/sidebar.php
@@ -33,7 +33,16 @@
 			<script type="text/javascript" src="http://www.google.com/cse/query_renderer.js"></script>
 			<div id="queries"></div>
 			<script src="http://www.google.com/cse/api/partner-pub-6477915256609367/cse/8658141932/queries/js?oe=UTF-8&amp;callback=(new+PopularQueryRenderer(document.getElementById(%22queries%22))).render"></script>
-
+			<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+			<!-- witselzz -->
+			<ins class="adsbygoogle"
+				 style="display:block"
+				 data-ad-client="ca-pub-6477915256609367"
+				 data-ad-slot="9576471935"
+				 data-ad-format="auto"></ins>
+			<script>
+				(adsbygoogle = window.adsbygoogle || []).push({});
+			</script>
 			<?php dynamic_sidebar( 'themonic-sidebar' ); ?>
 		</div><!-- #secondary -->
 	<?php else : ?>	
